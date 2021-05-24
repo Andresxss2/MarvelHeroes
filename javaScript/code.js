9dbf02e2474bb4cce13acd1e33a572dc55752378
Andres Rojas <carlos.rojas4538gmail.com>
2021-05-24 13:41:08 -0500

    all search works god

--- javaScript/code.js
+++ javaScript/code.js
@@ -5,54 +5,123 @@ const   public_key='fe473f988e6a09d2d31fd7a529f0aaa2',
         url_characters='http://gateway.marvel.com/v1/public/characters',
         url_comics='http://gateway.marvel.com/v1/public/comics',
         content_comics=document.querySelector('#content_comics'),
-        searh_comic=document.querySelector('#search')
+        searh=document.querySelector('#search'),
+        add_serch=document.querySelector('#search_favorites')
         
 
+
+
+// capture all data for user view 
 const all_data = results =>{
-    const comic_img=`${results.thumbnail.path}/portrait_uncanny.${results.thumbnail.extension}`
-    const comic=` 
-    <h3 class="comic_title">Title: ${results.title}</h3>
-    <img src="${comic_img}" alt=${results.title}>
-    <div class="show_comics">
-        <button value=${results.id} id="favorite_comic" class="btn">
-                            <i class="fas fa-heart"></i>
-        </button>
-    </div>
-    <h4>about :</h4>
-    <p>${results.description}</p>
-    <h4>list characters in this comic :</h4>
-    <p>${results.characters}</p>
-    <h4>list variantes in this comic :</h4>
-    <p>${results.variants.name}</p>
-    `;
-    // console.log(results )
-    content_comics.insertAdjacentHTML('afterbegin', comic)
+    const comic_img=`${results.thumbnail.path}/portrait_uncanny.${results.thumbnail.extension}`,
+            comic=` 
+            <h3 class="comic_title">Title: ${results.title}</h3>
+            <img src="${comic_img}" alt=${results.title}>
+            <div class="show_comics">
+                <button value=${results.id} id="favorite_comic" class="btn">
+                                    <i class="fas fa-heart"></i>
+                </button>
+            </div>
+            <h4>about :</h4>
+            <p>${results.description}</p>
+
+            `;
+            // console.log(results )
+            content_comics.insertAdjacentHTML('afterbegin', comic)
 }
 
-const get_appi_marvel=()=>{
-
-    const date_ts= Date.now(),
-        hash = md5(date_ts+private_key+public_key),
-        url=`${url_comics}?ts=${date_ts}&apikey=${public_key}&hash=${hash}`
-
+const _fech_app_process=(url, funtion_action)=>{
     fetch(url)
     .then(res => res.json())
     .then(comics=> {
-        console.log(comics.data.results.variants)
+        
         comics.data.results.forEach(result => {
-            //console.log(result)
-            all_data(result)
+            funtion_action(result)
         })})
-    // .then(variants=>{
-    //     const comit_variants=variants.data.result.variants
-    //     for (const comit_variant of comit_variants) {
-    //         console.log(comit_variant)
-    //     }
-    // })
-    .catch(err => console.error(err))
+        .catch(err => console.error(err))
+}
+
+const get_appi_marvel=()=>{
+    const date_ts= Date.now(),
+    hash = md5(date_ts+private_key+public_key),
+    url=`${url_comics}?ts=${date_ts}&apikey=${public_key}&hash=${hash}`
+    _fech_app_process(url,all_data)
 }
 get_appi_marvel()
 
+// capture when user serch a comic
+// ?nameStartsWith=iron%20man&orderBy=name
+const serch_data = results =>{
+    if(results==="lost_data"){
+        
+        const comic=`<p>No se encontro tu busqueda, intenta con otro nombre</p>`;
+        
+        add_serch.insertAdjacentHTML('afterbegin', comic)
+    }
+    else if(typeof(results.title)==="undefined"){
+       
+        const comic_img=`${results.thumbnail.path}/portrait_uncanny.${results.thumbnail.extension}`,
+            comic=`
+            
+                <div class="show_comics_add">
+                    <h3 class="comic_title">${results.name}</h3>
+                    <img src="${comic_img}" alt=${results.name}>
+                    <button id="favorite_comics" class="btn">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                    <h4>about :</h4>
+                    <p>${results.description}</p>
+                    
+                </div>`;    
+        add_serch.insertAdjacentHTML('afterbegin', comic)
+    }
+    else{
+        const comic_img=`${results.thumbnail.path}/portrait_uncanny.${results.thumbnail.extension}`,
+        comic=`
+        
+            <div class="show_comics_add">
+                <h3 class="comic_title">${results.title}</h3>
+                <img src="${comic_img}" alt=${results.title}>
+                <button id="favorite_comics" class="btn">
+                    <i class="fas fa-heart"></i>
+                </button>
+                <h4>about :</h4>
+                <p>${results.description}</p>
+                
+            </div>
+        
+        `;    
+        add_serch.insertAdjacentHTML('afterbegin', comic)
+    }   
+
+}
+
+const search_comics = name =>{
+    const   date_ts= Date.now(),
+            hash = md5(date_ts+private_key+public_key),
+            comic=encodeURIComponent(name),
+            search_comics_url=`${url_comics}?format=comic&formatType=comic&noVariants=true&titleStartsWith=${comic}&orderBy=title&ts=${date_ts}&apikey=${public_key}&hash=${hash}`,
+            search_character_url=`${url_characters}?nameStartsWith=${comic}&orderBy=name&ts=${date_ts}&apikey=${public_key}&hash=${hash}`
+
+    // get comics data
+    _fech_app_process(search_comics_url,serch_data)
+
+    //get characters data
+     _fech_app_process(search_character_url,serch_data)
+    
+}
+
+searh.addEventListener('keyup', key=>{
+    if(key.keyCode === 13){
+        add_serch.innerHTML=''
+        search_comics(key.target.value.trim().replace(' ','-'))
+        
+    }
+})
+        
+
+
+//capute when user cliked on bar menu
 
 const bars_menu=()=>{
        console.log('Soy bar menu')
@@ -70,7 +139,6 @@ const bars_menu=()=>{
             add_exit.classList.add('fa-bars')
        }
 }
-
 const bnt_favorite=()=>{
     console.log("Soy fav")
 
